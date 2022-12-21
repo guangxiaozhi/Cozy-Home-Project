b4409609c8191b5fd3499ef4455047aeacc249bd
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2022-12-21 08:37:12 -0800

    update the database

--- backend/db/migrations/20221220012741-create-review-image.js
+++ backend/db/migrations/20221220012741-create-review-image.js
@@ -33,9 +33,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       }
-    });
+    },options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ReviewImages');
+    options.tableName = 'ReviewImages'
+    await queryInterface.dropTable(options);
   }
 };
