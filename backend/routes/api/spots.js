48e166ae1b820677ca9462f0b5a38986755141d2
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2022-12-21 17:04:11 -0800

    create a spot, unfinished

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,8 @@ const express = require('express');
 const sequelize = require('sequelize')
 const router = express();
 const { Spot, Review, SpotImage } = require('../../db/models');
-const { requireAuth } = require("../../utils/auth");
+const { requireAuth ,handleValidationErrors} = require("../../utils/auth");
+const { check} = require("express-validator");
 
 
 router.get('/', requireAuth, async (req, res, next)=> {
@@ -82,6 +83,48 @@ router.get('/current', requireAuth, async (req, res, next) =>{
 
   res.json({Spots});
 })
+const validateSpot = [
+  check('address')
+    .notEmpty()
+    .withMessage('Street address is required'),
+  check('city')
+  .notEmpty()
+  .withMessage('City is required.'),
+  check('state')
+  .notEmpty()
+  .withMessage('State is required.'),
+  check('country')
+  .notEmpty()
+  .withMessage('Country is required.'),
+  check('lat')
+  .notEmpty()
+  .isDecimal()
+  .withMessage('Latitude is not valid'),
+  check('lng')
+  .notEmpty()
+  .isDecimal()
+  .withMessage('Longitude is not valid'),
+  check('name')
+  .isLength({ max: 50 })
+  .withMessage('Name must be less than 50 characters')
+  .notEmpty()
+  .withMessage('Name is required'),
+  check('description')
+  .notEmpty()
+  .withMessage('Description is required'),
+  check('price')
+  .notEmpty()
+  .isDecimal()
+  .withMessage('Price per day is required'),
+  handleValidationErrors
+];
+router.post('/', requireAuth,validateSpot, async (req, res, next)=>{
+  const {user} = req;
+  console.log(user);
+  const {address, city, state, country, lat, lng, name ,description, price} = req.body;
+  const spot = await user.createSpot({ownerId:user.id,address, city, state, country, lat, lng, name ,description, price});
+  res.json(spot);
 
+})
 
 module.exports = router;
