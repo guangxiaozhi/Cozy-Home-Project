0330f485d449226fac45cdbdb27fefc3794b7dc2
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2022-12-28 06:08:46 -0800

    Delete a Review Image

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const spotsRouter = require('./spots.js');
 const reviewRouter = require('./reviews.js')
 const bookingRouter = require('./bookings.js')
 const spotimagesRouter = require('./spotimages.js')
+const reviewimagesRouter = require('./reviewimages.js')
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
@@ -15,6 +16,7 @@ router.use('/spots', spotsRouter);
 router.use('/reviews', reviewRouter);
 router.use('/bookings', bookingRouter);
 router.use('/spot-images', spotimagesRouter);
+router.use('/review-images', reviewimagesRouter);
 
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
---
+++ backend/routes/api/reviewimages.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express();
+const { Review, ReviewImage } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth.js');
+
+router.delete('/:id', requireAuth, async (req, res, next)=> {
+  const reviewImage = await ReviewImage.findByPk(req.params.id);
+  if(!reviewImage){
+    res.status(404);
+    return res.json({
+      "message": "Review Image couldn't be found"
+    })
+  }
+  const review = await Review.findByPk(reviewImage.reviewId);
+  if(review.userId !== req.user.id){
+    res.status(403);
+    return res.json({
+      "message": "Forbidden"
+    })
+  }else{
+    await reviewImage.destroy();
+    res.status(200);
+    return res.json({
+      "message": "Successfully deleted"
+    })
+  }
+})
+
+
+module.exports = router;
