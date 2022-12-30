80f715d0eb37aadf1e300f714db421cfe7df1f64
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2022-12-29 22:54:36 -0800

    fixed log in error message

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -11,10 +11,10 @@ const validateLogin = [
   check('credential')
     .exists({ checkFalsy: true })
     .notEmpty()
-    .withMessage('Please provide a valid email or username.'),
+    .withMessage('Email or username is required'),
   check('password')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a password.'),
+    .withMessage('Password is required'),
   handleValidationErrors
 ];
 router.post(
@@ -26,24 +26,30 @@ router.post(
     const user = await User.login({ credential, password });
 
     if (!user) {
-      const err = new Error('Login failed');
       //Origin error message:
-      // err.status = 401;
-      // err.title = 'Login failed';
-      // err.errors = ['The provided credentials were invalid.'];
-      // return next(err);
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
 
       //error message from api documentation
-      err.status = 401;
-      res.json({
-        "message": "Invalid credentials",
-      })
+      // res.status = 401;
+      // return res.json({
+      //   "message": "Invalid credentials",
+      // })
     }
 
     await setTokenCookie(res, user);
 
     return res.json({
-      user: user
+      "user": {
+        "id": user.id,
+        "firstName": user.firstName,
+        "lastName": user.lastName,
+        "email": user.email,
+        "username": user.username
+      }
     });
   }
 );
