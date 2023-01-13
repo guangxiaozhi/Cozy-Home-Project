ed3275f7d739238b56227a2d567b7569ffc8185e
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2023-01-13 00:52:18 -0800

    Wrapping fetch requests with CSRF

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 import configureStore from './store';
+
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
+
 
 function Root(){
   return (
@@ -20,7 +26,7 @@ function Root(){
         <App />
       </BrowserRouter>
     </Provider>
-  )
+  );
 }
 
 ReactDOM.render(
