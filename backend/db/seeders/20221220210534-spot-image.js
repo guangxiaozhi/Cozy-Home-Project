188d47389cbeae2ae5f793c256d1640560ff4d44
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2023-01-13 22:22:19 -0800

    add url

--- backend/db/seeders/20221220210534-spot-image.js
+++ backend/db/seeders/20221220210534-spot-image.js
@@ -8,22 +8,22 @@ if (process.env.NODE_ENV === 'production') {
 const spotImageSeeds = [
   {
     spotId:1,
-    url:'https://www.urbanrealm.com/images/buildings/building_1268.jpg',
+    url:'https://a0.muscache.com/im/pictures/950e2e18-1331-4e22-8026-a6f7a5de22a4.jpg?im_w=1440',
     preview:true
   },
   {
     spotId:2,
-    url:'spot2.png',
+    url:'https://a0.muscache.com/im/pictures/miso/Hosting-739807825225924253/original/b03c3578-0b65-43a5-b89d-da5d7bc8f559.jpeg?im_w=1200',
     preview:false
   },
   {
     spotId:3,
-    url:'spot3.png',
+    url:'https://a0.muscache.com/im/pictures/5716db99-af8e-4081-88ef-d33fe5728ef3.jpg?im_w=1200',
     preview:false
   },
   {
     spotId:4,
-    url:'spot4.png',
+    url:'https://a0.muscache.com/im/pictures/miso/Hosting-37019642/original/0ebde664-c9d0-43b1-bc6d-68571095ac5f.jpeg?im_w=1200',
     preview:true
   }
 ]
