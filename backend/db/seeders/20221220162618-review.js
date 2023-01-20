b756b43075b454ba6d3718d6a081fde628d6c3b5
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2023-01-19 19:12:30 -0800

    get reviews by spotId and Delete Review

--- backend/db/seeders/20221220162618-review.js
+++ backend/db/seeders/20221220162618-review.js
@@ -10,25 +10,25 @@ const reviewseed = [
   {
     spotId:1,
     userId:1,
-    review:"good",
+    review:"Most beautiful place you will ever stay at period.",
     stars:4
   },
   {
     spotId:2,
     userId:2,
-    review:"bad",
-    stars:1
+    review:"Such a beautiful location and everything we needed was accounted for. The host was amazing to communicate with. Will definitely look her up when we’re back!!",
+    stars:4
   },
   {
     spotId:3,
     userId:3,
-    review:"not good",
-    stars:2
+    review:"Small but lovely unit with everything you need even coffee and tea.",
+    stars:4
   },
   {
     spotId:4,
     userId:4,
-    review:"very good",
+    review:"This home is amazing and exactly how shown and described, although you can't really capture the beauty of the view and such through photos. Every window and every angle is/has an amazing view of the hills and the ocean. The home is very spacious and great for having guests over for dinner. Katherine (host) and her family are very friendly, super helpful, and great at ongoing communication. We will definitely stay here again in the future. It's a great place to bring your well-behaved furry friend as well.",
     stars:5
   }
 ]
